edd64f5ae864bff7b2754ac5c9f3f998608a0a18
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-06-01 19:47:11 +0900

    Create 10430.js

---
+++ solution/10430.js
@@ -0,0 +1,11 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split(' ');
+
+const A = parseInt(input[0]);
+const B = parseInt(input[1]);
+const C = parseInt(input[2]);
+
+console.log(`${(A+B)%C}
+${((A%C)+(B%C))%C}
+${(A*B)%C}
+${((A%C)*(B%C))%C}`);
