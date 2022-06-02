440c9b131eb842771b957dd1b7581d0469aa3489
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-06-03 01:26:09 +0900

    Create 2884.js

---
+++ solution/2884.js
@@ -0,0 +1,17 @@
+let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString();
+let input = fs.readFileSync('input.txt').toString().split(" ").map(Number);
+
+let H = input[0];
+let M = input[1];
+
+M -= 45;
+
+if(M < 0){
+  H -= 1;
+  M += 60;
+  if(H < 0) H = 23;
+  console.log(H, M)
+} else {
+  console.log(H, M)
+}
