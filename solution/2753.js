955f96387fff618d47be50c9d428fd4b5a7ea2b9
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-06-03 00:29:23 +0900

    Update 2753.js

--- solution/2753.js
+++ solution/2753.js
@@ -6,3 +6,10 @@ if(input % 4 === 0 && (input % 100 !== 0 || input % 400 === 0) ){
 }else {
   console.log(0);
 }
+
+
+// 삼항 연산자로 변경해서 사용할 수 있다.
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString();
+
+input % 4 === 0 && (input % 100 !== 0 || input % 400 === 0) ? console.log(1) : console.log(0);
