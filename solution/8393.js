6f6ccb69923a26bab8188471069f5639cf029f24
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-06-04 18:42:06 +0900

    Create 8393.js

---
+++ solution/8393.js
@@ -0,0 +1,10 @@
+let fs = require('fs');
+let input = Number(fs.readFileSync('/dev/stdin').toString().trim());
+
+let result = 0;
+
+for(let i = 1; i <= input; i++){
+  result += i
+}
+
+console.log(result)
